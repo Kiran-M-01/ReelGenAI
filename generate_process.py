a7278b41b6cc3af600b0a2ddd2d8e3a6931a1947
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-06-02 22:51:02 +0530

    created generate_process file to generate reels

---
+++ generate_process.py
@@ -0,0 +1,10 @@
+#this file will look for new folders inside the user_uploads folder and converts them to reel if they are not already converted
+def text_to_audio(folder):
+    pass
+def create_reel(folder):
+    pass
+
+if __name__ == "__main__":
+    folder = '23....'
+    text_to_audio(folder)
+    create_reel(folder)
