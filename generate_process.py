64741172266dcb9d75a4dc55885fc52329965e5c
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-06-04 18:28:06 +0530

    converting text to audio done

--- generate_process.py
+++ generate_process.py
@@ -1,6 +1,7 @@
 #this file will look for new folders inside the user_uploads folder and converts them to reel if they are not already converted
 import os
 from text_to_audio import text_to_speech_file
+import time
 
 def text_to_audio(folder):
     print("TTA - ",folder)
@@ -13,14 +14,27 @@ def create_reel(folder):
     print("CR - ",folder)
 
 if __name__ == "__main__":
-    with open("done.txt", "r") as f:
-        done_folders = f.readlines()
+    while True:
+        print("processing queue...")
+        with open("done.txt", "r") as f:
+            done_folders = f.readlines()
 
-    done_folders = [f.strip() for f in done_folders]
-    folders = os.listdir("user_uploads")
-    for folder in folders:
-        if(folder not in done_folders):
-            text_to_audio(folder)
-            create_reel(folder)
-            with open("done.txt", "a") as f:
-                f.write(folder + "\n")
+        done_folders = [f.strip() for f in done_folders]
+
+        folders = os.listdir("user_uploads")
+
+        print("Folders:", folders)
+        print("Done:", done_folders)
+
+        for folder in folders:
+            print("Checking:", folder)
+
+            if(folder not in done_folders):
+                print("Processing:", folder)
+
+                text_to_audio(folder)
+                create_reel(folder)
+
+                with open("done.txt", "a") as f:
+                        f.write(folder + "\n")
+        time.sleep(4)
