a851c360f835c43418cfefd11dd6badd1093bdda
TejasNayak-000 <mkiranmkiran306@gmail.com>
2026-07-08 02:27:27 +0530

    implemented sqlite to create user table

--- database.py
+++ database.py
@@ -1,3 +1,17 @@
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
-db = SQLAlchemy()
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+cursor.execute("""create table if not exists users (id INTEGER PRIMARY KEY AUTOINCREMENT, username varchar(100) UNIQUE NOT NULL, email varchar(100) UNIQUE NOT NULL, password_hash varchar(255) NOT NULL)""")
+
+
+
+# cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+# print(cursor.fetchall())
+
+connection.commit()
+connection.close()
+
+
+print("Users table created successfully!")
